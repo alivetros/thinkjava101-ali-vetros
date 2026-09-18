a2fcb4a96226cd36f75c602b66686cf7c1b51277
alivetros <avetros@gmail.com>
2026-09-18 01:01:35 +0000

    Update Week 01 starter

--- student-work/_template/week01/src/Week01HelloStarter.java
+++ student-work/_template/week01/src/Week01HelloStarter.java
@@ -1,8 +1,13 @@
 public class Week01HelloStarter {
 
     public static void main(String[] args) {
-        System.out.println("TODO: replace this with your name.");
+/*      System.out.println("TODO: replace this with your name.");
         System.out.println("TODO: add your course goal.");
         System.out.println("TODO: add one more line about yourself.");
+
+*/
+        System.out.println("Ali Vetros.");
+        System.out.println("To be a better programmer.");
+        System.out.println("I am excited to learn Java and improve my coding skills.");
     }
 }
